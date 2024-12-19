f139e4570315e0eb91a1e9c9a5704dfeb327fc7a
yysh79 <yysh79@gmail.com>
2024-12-19 18:00:20 +0200

    Update view.py

--- view.py
+++ view.py
@@ -4,6 +4,6 @@ from controller import on_button_click
 
 
 def main(page: ft.Page):
-    model = ButtonModel()
-    button = ft.ElevatedButton("click", bgcolor=model.colors[model.current_index], on_click= lambda e: on_button_click(e, model, button) )
+    model  = ButtonModel()
+    button :ft.ElevatedButton = ft.ElevatedButton("click", bgcolor=model.colors[model.current_index], on_click= lambda e: on_button_click(e, model, button) )
     page.add(button)
